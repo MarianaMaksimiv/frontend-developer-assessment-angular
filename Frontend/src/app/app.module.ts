5a82d6d74bc280f5d2493b49ba2d765ca6668a20
MarianaMaksimiv <m.maksimiv@gmail.com>
2023-03-14 14:07:33 +1300

    update solution

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,6 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule, MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { TodoEffects } from './todo/store/todo.effects';
+import { todoReducer } from './todo/store/todo.reducers';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: async () => (await import('./todo/todo.module')).TodoModule,
+  },
+  { path: '**', redirectTo: '/' },
+
+]
 
 @NgModule({
   declarations: [
@@ -10,9 +27,23 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    RouterModule.forRoot(routes),
+    StoreModule.forRoot({ todos: todoReducer }, {}),
+    EffectsModule.forRoot([TodoEffects])
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {
+        horizontalPosition: 'right',
+        verticalPosition: 'top',
+        duration: 3 * 1000,
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
